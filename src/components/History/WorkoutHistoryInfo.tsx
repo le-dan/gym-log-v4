d17662e0fce3486b509482209ddb394edf796ac6
mineult <tle57@gmu.edu>
2025-04-07 01:19:55 -0400

    fix build issues

--- src/components/History/WorkoutHistoryInfo.tsx
+++ src/components/History/WorkoutHistoryInfo.tsx
@@ -1,23 +1,5 @@
-import { useParams } from "react-router";
-import { WorkoutInterface } from "../../util/interfaces";
 import WorkoutComplete from "../Dashboard/WorkoutComplete";
-import { useEffect, useState } from "react";
-import { fetchData } from "../../util/util";
 
 export default function WorkoutHistoryInfo() {
-	const { completionDate } = useParams();
-
-	const [workoutHistory, setWorkoutHistory] = useState<WorkoutInterface[]>();
-
-	useEffect(() => {
-		fetchData<WorkoutInterface[]>("WorkoutHistory").then((res) => {
-			setWorkoutHistory(res);
-		});
-	}, []);
-
-	const workout: WorkoutInterface | undefined = workoutHistory?.find((w) => {
-		return w.completionDate?.toLowerCase() === completionDate;
-	});
-
-	return <WorkoutComplete workout={workout} archive />;
+	return <WorkoutComplete archive />;
 }
